1b1fbdcd8a76442fa0d1bda6a8d7f9310c69f431
Juanma Curbelo <juancurbelo02@gmail.com>
2024-09-13 10:59:21 -0300

    feat: add logout and options debug

--- src/SDK.js
+++ src/SDK.js
@@ -2,64 +2,98 @@ import ApiService from "./ApiService";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
-import messaging from '@react-native-firebase/messaging';
-import { Platform } from 'react-native';
-
+import messaging from "@react-native-firebase/messaging";
+import { Platform } from "react-native";
 
 class SDK {
-	async init(clientId, clientSecret) {
+	async init(clientId, clientSecret, options = {}) {
 		try {
-			await ApiService.init(clientId, clientSecret);
+			const response = await ApiService.init(clientId, clientSecret);
+			if (options.debug) {
+				console.log("SDK Token:", response.token);
+				console.log("SDK Session:", response.sessionId);
+			}
+			return response;
 		} catch (error) {
 			console.error("Error during SDK initialization:", error);
 		}
 	}
 
-	async login(customerId) {
+	async login(customerId, options = {}) {
 		try {
-			await ApiService.post("/channel/login", { customerId });
-			await AsyncStorage.setItem("gsVUID", customerId);
+			if (typeof customerId !== "undefined") {
+				await ApiService.post("/channel/login", { customerId });
+				await AsyncStorage.setItem("gsVUID", customerId);
+			}
+			if (options.debug) {
+				console.log("customerId", customerId);
+			}
 		} catch (error) {
 			console.error("Login failed:", error);
 			throw new Error("Failed to login");
 		}
 	}
 
-	async search(text) {
+	async logout(options = {}) {
+		try {
+			await ApiService.post("/channel/logout");
+			if (options.debug) {
+				console.log(await AsyncStorage.getItem("gsVUID"));
+			}
+			await AsyncStorage.removeItem("gsVUID");
+			const newGsVUID = uuidv4();
+			await AsyncStorage.setItem("gsVUID", newGsVUID);
+			if (options.debug) {
+				console.log("New gsVUID:", await AsyncStorage.getItem("gsVUID"));
+			}
+		} catch (error) {
+			console.error("Logout failed:", error);
+			throw new Error("Failed to logout");
+		}
+	}
+
+	async search(text, options = {}) {
 		try {
 			const response = await ApiService.get("/item/search", {
 				params: { query: text },
 			});
-
+			if (options.debug) {
+				console.log("Search response:", response.data);
+			}
 			return response.data;
 		} catch (error) {
 			console.error("Search failed:", error);
-			return []
+			return [];
 		}
 	}
 
-	async getContent(contentId) {
+	async getContent(contentId, options = {}) {
 		try {
 			const response = await ApiService.post(`/personal/content/${contentId}`);
+			if (options.debug) {
+				console.log("Content response:", response.data);
+			}
 			return response.data;
 		} catch (error) {
 			if (error.response && error.response.status === 404) {
 				return null;
-			} else {
-				console.error("Failed to get content:", error);
-				return null;
 			}
+			console.error("Failed to get content:", error);
+			return null;
 		}
 	}
 
-	async addInteraction(interactionName, data) {
+	async addInteraction(interactionName, data, options = {}) {
 		try {
 			const interactionData = {
 				event: interactionName,
 				...data,
 			};
-
-			return ApiService.post("/interaction", interactionData);
+			const response = await ApiService.post("/interaction", interactionData);
+			if (options.debug) {
+				console.log("Interaction response:", response);
+			}
+			return response;
 		} catch (error) {
 			console.error("Failed to add interaction:", error);
 			throw new Error("Failed to add interaction");
@@ -69,82 +103,135 @@ class SDK {
 	async addInteractionState(state, options = {}) {
 		try {
 			options.transactionId = uuidv4();
-			return await ApiService.post(`/interaction/state/${state}`, options);
+			const response = await ApiService.post(
+				`/interaction/state/${state}`,
+				options,
+			);
+			if (options.debug) {
+				console.log("Interaction state response:", response);
+			}
+			return response;
 		} catch (error) {
 			console.error("Failed to add interaction state:", error);
 			throw new Error("Failed to add interaction state");
 		}
 	}
 
-	async openImpression(impressionId) {
+	async openImpression(impressionId, options = {}) {
 		try {
-			await ApiService.patch(`/personal/impression/${impressionId}`, {
-				status: "opened",
-			});
+			const response = await ApiService.patch(
+				`/personal/impression/${impressionId}`,
+				{
+					status: "opened",
+				},
+			);
+			if (options.debug) {
+				console.log("Open impression response:", response);
+			}
 		} catch (error) {
 			console.error("Failed to open impression content:", error);
 			throw new Error("Failed to open impression content");
 		}
 	}
 
-	async initializeFirebase() {
-        try {
-            await messaging().registerDeviceForRemoteMessages();
-            console.log('Firebase messaging initialized');
-        } catch (error) {
-            console.error('Failed to initialize Firebase messaging:', error);
-            throw new Error('Failed to initialize Firebase messaging');
-        }
-    }
+	async initializeFirebase(options = {}) {
+		try {
+			await messaging().registerDeviceForRemoteMessages();
+			if (options.debug) {
+				console.log("Firebase messaging initialized");
+			}
+		} catch (error) {
+			console.error("Failed to initialize Firebase messaging:", error);
+			throw new Error("Failed to initialize Firebase messaging");
+		}
+	}
 
-    async requestNotificationPermission() {
-        try {
-            const authStatus = await messaging().requestPermission();
-            const enabled =
-                authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-                authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+	async requestNotificationPermission(options = {}) {
+		try {
+			if (Platform.OS === "android" && Platform.Version >= 33) {
+				const postNotificationsAndroidStatus = await PermissionsAndroid.request(
+					PermissionsAndroid.PERMISSIONS.POST_NOTIFICATION,
+				);
+				if (options.debug) {
+					console.log(
+						typeof Platform.Version,
+						postNotificationsAndroidStatus,
+						"androidStatus",
+					);
+				}
+				if (postNotificationsAndroidStatus === "granted") {
+					return true;
+				}
+				return false;
+			}
+			if (Platform.OS === "ios") {
+				const authStatus = await messaging().requestPermission();
+				const enabled =
+					authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+					authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-            if (enabled) {
-                console.log('Authorization status:', authStatus);
-                return true;
-            }
-            return false;
-        } catch (error) {
-            console.error('Failed to request notification permission:', error);
-            throw new Error('Failed to request notification permission');
-        }
-    }
+				if (enabled) {
+					if (options.debug) {
+						console.log("Authorization status:", authStatus);
+					}
+					return true;
+				}
+			}
+			return false;
+		} catch (error) {
+			console.error("Failed to request notification permission:", error);
+			throw new Error("Failed to request notification permission");
+		}
+	}
 
-    async getFirebaseToken() {
-        try {
-            const token = await messaging().getToken();
-            console.log('Firebase token:', token);
-            return token;
-        } catch (error) {
-            console.error('Failed to get Firebase token:', error);
-        }
-    }
+	async getFirebaseToken(options = {}) {
+		try {
+			const token = await messaging().getToken();
+			if (options.debug) {
+				console.log("Firebase token:", token);
+			}
+			return token;
+		} catch (error) {
+			console.error("Failed to get Firebase token:", error);
+		}
+	}
 
-    onMessage(callback) {
-        return messaging().onMessage(callback);
-    }
+	onMessage(callback, options = {}) {
+		if (options.debug) {
+			console.log("onMessage callback set");
+		}
+		return messaging().onMessage(callback);
+	}
 
-    onNotificationOpenedApp(callback) {
-        return messaging().onNotificationOpenedApp(callback);
-    }
+	onNotificationOpenedApp(callback, options = {}) {
+		if (options.debug) {
+			console.log("onNotificationOpenedApp callback set");
+		}
+		return messaging().onNotificationOpenedApp(callback);
+	}
 
-    async getInitialNotification() {
-        return await messaging().getInitialNotification();
-    }
+	async getInitialNotification(options = {}) {
+		const notification = await messaging().getInitialNotification();
+		if (options.debug) {
+			console.log("Initial notification:", notification);
+		}
+		return notification;
+	}
 
-	async sendTokenToBackend(token) {
-        try {
+	async sendTokenToBackend(token, options = {}) {
+		try {
 			const deviceType = Platform.OS;
-            await ApiService.post('/channel/notification-token', { token, deviceType });
-        } catch (error) {
-            console.error('Failed to send token to backend:', error);
-        }
-    }
+			const response = await ApiService.post("/channel/notification-token", {
+				token,
+				deviceType,
+			});
+			if (options.debug) {
+				console.log("Send token to backend response:", response);
+			}
+		} catch (error) {
+			console.error("Failed to send token to backend:", error);
+		}
+	}
 }
 
 export default new SDK();
